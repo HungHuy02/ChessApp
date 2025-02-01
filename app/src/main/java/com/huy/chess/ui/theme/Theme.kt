a3d8d9d80d52a155b03e3949edc80e05f147944a
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-02-01 21:43:29 +0700

    setup themes

--- app/src/main/java/com/huy/chess/ui/theme/Theme.kt
+++ app/src/main/java/com/huy/chess/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.huy.chess.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green300,
+    onPrimary = Color.White,
+    secondary = TransparentWhite10,
+    onSecondary = TransparentWhite72,
+    background = BackgroundColor,
+    surface = Gray900
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Green300,
+    onPrimary = Color.White,
+    secondary = Gray300,
+    onSecondary = Gray600,
+    background = BackgroundColor,
+    surface = Gray900
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -53,6 +59,7 @@ fun ChessTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
