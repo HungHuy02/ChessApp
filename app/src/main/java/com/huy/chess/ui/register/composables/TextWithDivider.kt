dd221f1e1e030fe46e0928c2f61449f81420a26b
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-02-03 21:15:52 +0700

    register screen

---
+++ app/src/main/java/com/huy/chess/ui/register/composables/TextWithDivider.kt
@@ -0,0 +1,39 @@
+package com.huy.chess.ui.register.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.huy.chess.R
+
+@Composable
+fun TextWithDivider(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        HorizontalDivider(modifier = Modifier.width(40.dp))
+        Text(
+            text = stringResource(R.string.or_text),
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        HorizontalDivider(modifier = Modifier.width(40.dp))
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    TextWithDivider()
+}
