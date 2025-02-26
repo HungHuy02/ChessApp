25612bfe93ef402444cd0d5047e193fd44139238
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-02-26 21:57:08 +0700

    datastore with hilt-dagger

---
+++ app/src/main/java/com/huy/chess/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.huy.chess.di
+
+import android.content.Context
+import com.huy.chess.data.preferences.abstraction.DataStoreRepository
+import com.huy.chess.data.preferences.implement.DataStoreRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideDataStoreRepository(
+        @ApplicationContext app: Context
+    ): DataStoreRepository = DataStoreRepositoryImpl(app)
+}
--- app/src/main/java/com/huy/chess/utils/Constants.kt
+++ app/src/main/java/com/huy/chess/utils/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     const val MAX_TIME_SELECT = 120f
     const val MAX_BONUS_TIME_SELECT = 60f
+
+    const val DATASTORE_TWO_PEOPLE_BUTTON_SELECTED = "button_two_people"
+    const val DATASTORE_CHANGE_TIME_BUTTON_SELECTED = "button_change_time"
+    const val DATASTORE_BOT_BUTTON_SELECTED = "button_bot"
 }
