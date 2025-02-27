56ab67bda0107df0d5b3a89198bcf4d4357e519a
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-02-27 22:21:04 +0700

    update library, setup coil and retrofit2 with moshi

--- app/src/main/java/com/huy/chess/ChessApplication.kt
+++ app/src/main/java/com/huy/chess/ChessApplication.kt
@@ -1,7 +1,27 @@
 package com.huy.chess
 
 import android.app.Application
+import android.content.Context
+import android.graphics.Bitmap
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.request.bitmapConfig
+import coil3.request.crossfade
+import com.huy.chess.data.network.RetrofitClient
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ChessApplication : Application()
+class ChessApplication : Application(), SingletonImageLoader.Factory {
+
+    override fun onCreate() {
+        super.onCreate()
+        RetrofitClient.createClient(this)
+    }
+
+    override fun newImageLoader(context: Context): ImageLoader {
+        return ImageLoader.Builder(context)
+            .bitmapConfig(Bitmap.Config.ARGB_8888)
+            .crossfade(true)
+            .build()
+    }
+}
