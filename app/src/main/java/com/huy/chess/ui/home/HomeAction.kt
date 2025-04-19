cb8fe58d18632f3ab52b5a3de21d753aa948770a
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-04-19 11:37:58 +0700

    feat: update home screen ui and action

---
+++ app/src/main/java/com/huy/chess/ui/home/HomeAction.kt
@@ -0,0 +1,10 @@
+package com.huy.chess.ui.home
+
+sealed class HomeAction {
+    data object ClickedDailyPuzzle: HomeAction()
+    data object ClickedPlayOnline: HomeAction()
+    data object ClickedPuzzle: HomeAction()
+    data object ClickedBot: HomeAction()
+    data object ClickedStudy: HomeAction()
+    data object ClickedPlay: HomeAction()
+}
---
+++ app/src/main/java/com/huy/chess/ui/home/HomeState.kt
@@ -0,0 +1,5 @@
+package com.huy.chess.ui.home
+
+data class HomeState(
+    val isLogin: Boolean = false
+)
