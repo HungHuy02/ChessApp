423d5d702f935622f75c202b116f73c5ab220992
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-04-28 22:13:35 +0700

    feat: captured piece

---
+++ app/src/main/java/com/huy/chess/ui/play/composables/CapturedPiece.kt
@@ -0,0 +1,92 @@
+package com.huy.chess.ui.play.composables
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import com.huy.chess.ui.component.getChessPiecePainters
+import com.huy.chess.utils.increment
+
+@Composable
+fun CapturedPiece(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+    val density = LocalDensity.current.density
+    val boardSize = (configuration.screenWidthDp * density).toInt()
+    val cellSize = boardSize / 12
+    val pieceSize = cellSize / 3f
+    val map = mutableMapOf<Char, Int>()
+    map.increment('p')
+    map.increment('q')
+    map.increment('P')
+    map.increment('N')
+    val list = remember { getChessPiecePainters(context, cellSize) }
+    Canvas(modifier = modifier) {
+        var currentX = 0f
+        listOf('p', 'n', 'b', 'r', 'q').forEach { pieceType ->
+            map[pieceType]?.let { count ->
+                currentX = drawPiece(
+                    count = count,
+                    imageBitmap = list[getPieceDrawableId(pieceType)],
+                    x = currentX,
+                    y = 0f,
+                    distance = pieceSize
+                )
+            }
+        }
+        currentX = 0f
+        listOf('P', 'N', 'B', 'R', 'Q').forEach { pieceType ->
+            map[pieceType]?.let { count ->
+                currentX = drawPiece(
+                    count = count,
+                    imageBitmap = list[getPieceDrawableId(pieceType)],
+                    x = currentX,
+                    y = cellSize * 2f,
+                    distance = pieceSize
+                )
+            }
+        }
+    }
+}
+
+fun DrawScope.drawPiece(
+    count: Int,
+    imageBitmap: ImageBitmap,
+    x: Float,
+    y: Float,
+    distance: Float
+) : Float {
+    var positionX = x + distance
+    for (i in 0 .. count) {
+        drawImage(
+            image = imageBitmap,
+            topLeft = Offset(positionX + distance * i, y)
+        )
+    }
+    return positionX
+}
+
+private fun getPieceDrawableId(piece: Char): Int {
+    return when (piece) {
+        'p' -> 0
+        'r' -> 1
+        'b' -> 2
+        'n' -> 3
+        'q' -> 4
+        'k' -> 5
+        'P' -> 6
+        'R' -> 7
+        'B' -> 8
+        'N' -> 9
+        'Q' -> 10
+        else -> 11
+    }
+}
