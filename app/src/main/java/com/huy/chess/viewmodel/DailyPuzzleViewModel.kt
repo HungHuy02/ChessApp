59c87ad717f1ecb52eb8d41aafba3295d345bd8d
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-05-16 16:35:29 +0700

    feat: change get data from api to local cache

--- app/src/main/java/com/huy/chess/viewmodel/DailyPuzzleViewModel.kt
+++ app/src/main/java/com/huy/chess/viewmodel/DailyPuzzleViewModel.kt
@@ -1,34 +1,35 @@
 package com.huy.chess.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.viewModelScope
 import com.huy.chess.base.BaseViewModel
-import com.huy.chess.data.network.repository.PuzzleRepository
+import com.huy.chess.data.preferences.dailyPuzzleDataStore
 import com.huy.chess.ui.component.parseFen
 import com.huy.chess.ui.dailypuzzle.DailyPuzzleAction
 import com.huy.chess.ui.dailypuzzle.DailyPuzzleEffect
 import com.huy.chess.ui.dailypuzzle.DailyPuzzleState
-import com.huy.chess.utils.Utils
 import com.huy.chess.utils.enums.PuzzleStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DailyPuzzleViewModel @Inject constructor(private val puzzleRepository: PuzzleRepository) : BaseViewModel<DailyPuzzleState, DailyPuzzleAction, DailyPuzzleEffect>(DailyPuzzleState()) {
+class DailyPuzzleViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+) : BaseViewModel<DailyPuzzleState, DailyPuzzleAction, DailyPuzzleEffect>(DailyPuzzleState()) {
 
     init {
         viewModelScope.launch {
-            val date = Utils.getToday()
-            puzzleRepository.getDailyPuzzle(date)
-                .onSuccess { puzzle ->
-                    val side = parseFen(puzzle.fen)
-                    updateState { it.copy(
-                        title = puzzle.title,
-                        date = puzzle.date,
-                        fen = puzzle.fen,
-                        moves = puzzle.moves.split(" "),
-                        puzzleStatus = if(side) PuzzleStatus.START_WHITE else PuzzleStatus.START_BLACK
-                    ) }
+            context.dailyPuzzleDataStore.data.collect {puzzle ->
+                val side = parseFen(puzzle.fen)
+                updateState { it.copy(
+                    title = puzzle.title,
+                    date = puzzle.date,
+                    fen = puzzle.fen,
+                    moves = puzzle.moves.split(" "),
+                    puzzleStatus = if(side) PuzzleStatus.START_WHITE else PuzzleStatus.START_BLACK
+                ) }
             }
         }
     }
