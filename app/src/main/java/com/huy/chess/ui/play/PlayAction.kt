61f6dc26c4df9c3c29bb8dfbf2f89ea9503a8779
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2025-05-17 22:37:10 +0700

    feat: update play screen

--- app/src/main/java/com/huy/chess/ui/play/PlayAction.kt
+++ app/src/main/java/com/huy/chess/ui/play/PlayAction.kt
@@ -2,7 +2,7 @@ package com.huy.chess.ui.play
 
 sealed class PlayAction {
     data object ClickedMore: PlayAction()
-    data object ClickedAdd: PlayAction()
+    data object ClickedAnalysis: PlayAction()
     data object ClickedBack: PlayAction()
     data object ClickedForward: PlayAction()
     data object ClickedBackButton: PlayAction()
--- app/src/main/java/com/huy/chess/ui/play/composables/PlayScreenBottomBar.kt
+++ app/src/main/java/com/huy/chess/ui/play/composables/PlayScreenBottomBar.kt
@@ -1,98 +1,77 @@
 package com.huy.chess.ui.play.composables
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 import com.huy.chess.R
 import com.huy.chess.ui.play.PlayAction
+import com.huy.chess.ui.theme.ChessGlyphFontFamily
 
 @Composable
 fun PlayScreenBottomBar(
     modifier: Modifier = Modifier,
+    isEnd: Boolean = false,
     onClick: (PlayAction) -> Unit
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
     ) {
-        PlayScreenBottomBarItem.itemList.forEach {
+        PlayScreenBottomBarItem(
+            icon = "\u1F85",
+            text = stringResource(R.string.options_text),
+            onClick = { onClick(PlayAction.ClickedMore) }
+        )
+        if(isEnd)
             PlayScreenBottomBarItem(
-                painter = painterResource(it.icon),
-                text = stringResource(it.label),
-                onClick = { onClick(it.playAction) },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(vertical = 8.dp)
+                icon = "\u0394",
+                text = stringResource(R.string.analysis_text),
+                onClick = { onClick(PlayAction.ClickedAnalysis) }
             )
-        }
+        PlayScreenBottomBarItem(
+            icon = "\u002C",
+            text = stringResource(R.string.back_text),
+            onClick = { onClick(PlayAction.ClickedBack) }
+        )
+        PlayScreenBottomBarItem(
+            icon = "\u2026",
+            text = stringResource(R.string.forward_text),
+            onClick = { onClick(PlayAction.ClickedForward) }
+        )
     }
 }
 
 @Composable
-fun PlayScreenBottomBarItem(
+fun RowScope.PlayScreenBottomBarItem(
     modifier: Modifier = Modifier,
-    painter: Painter,
+    icon: String,
     text: String,
     onClick: () -> Unit
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.clickable {
-            onClick()
-        }
-    ) {
-        Icon(
-            painter = painter,
-            contentDescription = "icon"
-        )
-        Text(
-            text = text
-        )
-    }
-}
-
-sealed class PlayScreenBottomBarItem(
-    @DrawableRes val icon: Int,
-    @StringRes val label: Int,
-    val playAction: PlayAction
-) {
-    data object Options : PlayScreenBottomBarItem(
-        icon = R.drawable.format_list_bulleted_24px,
-        label = R.string.more_options_text,
-        playAction = PlayAction.ClickedMore
+    Text(
+        text = buildAnnotatedString {
+            withStyle(SpanStyle(fontFamily = ChessGlyphFontFamily, fontSize = 24.sp)) {
+                appendLine(icon)
+            }
+            withStyle(SpanStyle()) {
+                append(text)
+            }
+        },
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .weight(1f)
+            .clickable {
+                onClick()
+            }
     )
-
-    data object Add : PlayScreenBottomBarItem(
-        icon = R.drawable.add_24px,
-        label = R.string.more_options_text,
-        playAction = PlayAction.ClickedAdd
-    )
-
-    data object Back : PlayScreenBottomBarItem(
-        icon = R.drawable.chevron_left_24px,
-        label = R.string.back_text,
-        playAction = PlayAction.ClickedBack
-    )
-
-    data object Forward : PlayScreenBottomBarItem(
-        icon = R.drawable.chevron_right_24px,
-        label = R.string.forward_text,
-        playAction = PlayAction.ClickedForward
-    )
-
-    companion object {
-        val itemList = listOf(Options, Add, Back, Forward)
-    }
 }
